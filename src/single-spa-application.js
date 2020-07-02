c4d7146a227797dedeaad5894547d46589edc554
Mike Doubintchik <michael.doubintchik@bcbsnc.com>
2020-07-02 13:35:02 -0400

    updated single-spa config

--- src/single-spa-application.js
+++ src/single-spa-application.js
@@ -1,29 +1,57 @@
-System.register([], function(_export, _context) {
-  window.accountSettingsBaseDir = _context.meta.url.slice(0, _context.meta.url.lastIndexOf('/') + 1);
+// System.register([], function (_export, _context) {
+//   window.accountSettingsBaseDir = _context.meta.url.slice(
+//     0,
+//     _context.meta.url.lastIndexOf("/") + 1
+//   );
 
-  return {
-    execute() {
-      const link = document.createElement('link')
-      link.rel = 'stylesheet'
-      link.href = accountSettingsBaseDir + 'account-settings.css'
-      document.head.appendChild(link)
+//   return {
+//     execute() {
+//       const link = document.createElement("link");
+//       link.rel = "stylesheet";
+//       link.href = accountSettingsBaseDir + "account-settings.css";
+//       document.head.appendChild(link);
 
-      angular.module('account-settings').config(['$sceDelegateProvider', function ($sceDelegateProvider) {
-        $sceDelegateProvider.resourceUrlWhitelist([
-          'self',
-          window.accountSettingsBaseDir + '**',
-        ]);
-      }])
+//       angular.module("account-settings").config([
+//         "$sceDelegateProvider",
+//         function ($sceDelegateProvider) {
+//           $sceDelegateProvider.resourceUrlWhitelist([
+//             "self",
+//             window.accountSettingsBaseDir + "**",
+//           ]);
+//         },
+//       ]);
 
-      const angularjsLifecycles = singleSpaAngularjs.default({
-        angular: window.angular,
-        mainAngularModule: 'account-settings',
-        uiRouter: true,
-        preserveGlobal: true,
-        template: '<account-settings />'
-      });
+//       const angularjsLifecycles = singleSpaAngularjs.default({
+//         angular: window.angular,
+//         mainAngularModule: "account-settings",
+//         uiRouter: true,
+//         preserveGlobal: true,
+//         template: "<account-settings />",
+//       });
 
-      _export(angularjsLifecycles);
-    }
-  }
-})
+//       _export(angularjsLifecycles);
+//     },
+//   };
+// });
+
+window.BCMLegacy = singleSpaAngularjs({
+  angular: window.angular,
+  mainAngularModule: "account-settings",
+  uiRouter: true,
+  preserveGlobal: false,
+  template: "<account-settings />",
+});
+
+angular.module("account-settings").config([
+  "$sceDelegateProvider",
+  function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      "self",
+      window.accountSettingsBaseDir + "**",
+    ]);
+  },
+]);
+
+export const bootstrap = window.BCMLegacy.bootstrap;
+export const mount = window.BCMLegacy.mount;
+export const unmount = window.BCMLegacy.unmount;
