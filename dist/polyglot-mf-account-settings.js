7d4688fff70d310623de7171165be4b7b7a36efb
Mike Doubintchik <michael.doubintchik@bcbsnc.com>
2020-07-02 15:03:24 -0400

    updates

--- dist/polyglot-mf-account-settings.js
+++ dist/polyglot-mf-account-settings.js
@@ -47295,45 +47295,10 @@ $provide.value("$locale", {
   }
 });
 //# sourceMappingURL=single-spa-angularjs.js.map
-// System.register([], function (_export, _context) {
-//   window.accountSettingsBaseDir = _context.meta.url.slice(
-//     0,
-//     _context.meta.url.lastIndexOf("/") + 1
-//   );
-
-//   return {
-//     execute() {
-//       const link = document.createElement("link");
-//       link.rel = "stylesheet";
-//       link.href = accountSettingsBaseDir + "account-settings.css";
-//       document.head.appendChild(link);
-
-//       angular.module("account-settings").config([
-//         "$sceDelegateProvider",
-//         function ($sceDelegateProvider) {
-//           $sceDelegateProvider.resourceUrlWhitelist([
-//             "self",
-//             window.accountSettingsBaseDir + "**",
-//           ]);
-//         },
-//       ]);
-
-//       const angularjsLifecycles = singleSpaAngularjs.default({
-//         angular: window.angular,
-//         mainAngularModule: "account-settings",
-//         uiRouter: true,
-//         preserveGlobal: true,
-//         template: "<account-settings />",
-//       });
-
 import angular from "angular";
-//       _export(angularjsLifecycles);
-//     },
-//   };
-// });
 import singleSpaAngularJS from "single-spa-angularjs";
 
-window.BCMLegacy = singleSpaAngularJS({
+const BCMLegacy = singleSpaAngularJS({
   angular,
   mainAngularModule: "account-settings",
   uiRouter: true,
@@ -47341,19 +47306,9 @@ window.BCMLegacy = singleSpaAngularJS({
   template: "<account-settings />",
 });
 
-angular.module("account-settings").config([
-  "$sceDelegateProvider",
-  function ($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist([
-      "self",
-      window.accountSettingsBaseDir + "**",
-    ]);
-  },
-]);
-
-export const bootstrap = window.BCMLegacy.bootstrap;
-export const mount = window.BCMLegacy.mount;
-export const unmount = window.BCMLegacy.unmount;
+export const bootstrap = BCMLegacy.bootstrap;
+export const mount = BCMLegacy.mount;
+export const unmount = BCMLegacy.unmount;
 
 angular.module('account-settings', [
   'ui.router'
